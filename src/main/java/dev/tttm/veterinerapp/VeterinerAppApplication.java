96568bc396a738dce4c5c5c274d03bb24e1cc1a4
tttmdev <muhammetmeminarikan@gmail.com>
2022-09-20 13:55:22 +0300

    changes

--- src/main/java/dev/tttm/veterinerapp/VeterinerAppApplication.java
+++ src/main/java/dev/tttm/veterinerapp/VeterinerAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VeterinerAppApplication {
-
+// git
     public static void main(String[] args) {
         SpringApplication.run(VeterinerAppApplication.class, args);
     }
